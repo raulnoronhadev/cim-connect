cd11e7e8e90f1ccfc35997589e6bcfc9c9d8bdff
raul <raulnoronhadev@gmail.com>
2025-12-12 17:38:47 -0300

    Backend project init

---
+++ backend/src/test/java/com/backend/cimconnect/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.cimconnect.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
