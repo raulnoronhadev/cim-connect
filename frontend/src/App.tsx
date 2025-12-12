63ba4842a146ff31b16c250dffdffd126cc0f5f2
raul <raulnoronhadev@gmail.com>
2025-12-12 17:37:14 -0300

    Initialized backend project.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
-import Profile from './Profile';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 
 function App() {
   const { isAuthenticated, isLoading, error } = useAuth0();
