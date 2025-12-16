22c98e149301130dea572f096072a6895ce71cde
raul <raulnoronhadev@gmail.com>
2025-12-15 22:50:29 -0300

    Created endpoint to get onel specific user (getOneUser).

--- backend/src/main/java/com/backend/cimconnect/controllers/UserController.java
+++ backend/src/main/java/com/backend/cimconnect/controllers/UserController.java
@@ -8,12 +8,10 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -31,5 +29,11 @@ public class UserController {
     @GetMapping("/users")
     public ResponseEntity<List<UserModel>> getAllUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value="id") UUID id) {
+        Optional<UserModel> user0 = userRepository.findById(id);
+        return user0.<ResponseEntity<Object>>map(userModel -> ResponseEntity.status(HttpStatus.OK).body(userModel)).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found"));
     }
 }
