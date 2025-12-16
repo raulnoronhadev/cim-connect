5e6528afdd6fcf11c54b45f26c7bf93a91ad67ed
raul <raulnoronhadev@gmail.com>
2025-12-16 15:29:22 -0300

    Added and implemented Lombok

--- backend/src/main/java/com/backend/cimconnect/controllers/UserController.java
+++ backend/src/main/java/com/backend/cimconnect/controllers/UserController.java
@@ -1,17 +1,23 @@
 package com.backend.cimconnect.controllers;
-import com.backend.cimconnect.dtos.UserRecordDto;
-import com.backend.cimconnect.models.UserModel;
-import com.backend.cimconnect.repositories.UserRepository;
-import jakarta.validation.Valid;
-import org.apache.catalina.User;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.cimconnect.dtos.UserRecordDto;
+import com.backend.cimconnect.models.UserModel;
+import com.backend.cimconnect.repositories.UserRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class UserController {
--- backend/src/main/java/com/backend/cimconnect/models/UserModel.java
+++ backend/src/main/java/com/backend/cimconnect/models/UserModel.java
@@ -7,9 +7,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 
 @Entity
 @Table(name = "TB_USERS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class UserModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -22,60 +28,4 @@ public class UserModel implements Serializable {
     private String church;
     private String pastor;
     private String role;
-
-    public UUID getIdUser() {
-        return idUser;
-    }
-
-    public void setIdUser(UUID idUser) {
-        this.idUser = idUser;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getChurch() {
-        return church;
-    }
-
-    public void setChurch(String church) {
-        this.church = church;
-    }
-
-    public String getPastor() {
-        return pastor;
-    }
-
-    public void setPastor(String pastor) {
-        this.pastor = pastor;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
